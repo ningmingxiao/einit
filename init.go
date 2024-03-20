35f11678be095abc724e54003e7ad07b2cd117f2
ningmingxiao <89581092+ningmingxiao@users.noreply.github.com>
2024-03-20 18:02:44 +0800

    Create init.go

---
+++ init.go
@@ -0,0 +1,50 @@
+package einit
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func reapZombies() error {
+	//static void sigreap(int signo) {
+	//	while (waitpid(-1, NULL, WNOHANG) > 0)
+	//	;
+	//}
+	wstatus := syscall.WaitStatus(0)
+	for {
+		currentChildPid, err := syscall.Wait4(-1, &wstatus, syscall.WNOHANG, nil)
+		if err != nil {
+			return err
+		}
+		if currentChildPid > 0 {
+			fmt.Printf("pid is %d \n", currentChildPid)
+			continue
+		} else if currentChildPid == 0 {
+			return nil
+		}
+	}
+}
+
+func waitSignal() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGCHLD)
+	go func() {
+		for {
+			sig := <-sigChan
+			if sig == syscall.SIGCHLD {
+				err := reapZombies()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}()
+}
+
+func ReapZombiePid() {
+	go func() {
+		waitSignal()
+	}()
+}
